52eb29452077a7c815a5ad8d5d4d68fd798b51d8
Scott Stanfield <scottstanfield@gmail.com>
2019-04-21 20:01:43 -0700

    Updated to use .env file

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+require('dotenv').config();
+const port = process.env.PORT || 8080;
+
+// setup logging maybe
+process.env.LOGGER && app.use(morgan(process.env.LOGGER));
+
+// trust the nginx proxy
+app.set('trust proxy', true);
+
+// only route
+app.get('/', (req, res) => {
+    res.send(`Hi Ben! I got HTTPS working: crazy stuff. Running express on ${port}`);
+})
+
+app.listen(port, () => {
+    console.log(`Running on ${port}.`);
+})
 
 
