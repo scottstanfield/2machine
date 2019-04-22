b37931df9d78b2f6a58dfe3c1d93bdd8aee9ada8
Scott Stanfield <scottstanfield@gmail.com>
2019-04-21 23:09:19 -0700

    Added pino

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const express = require('express');
-const app = express();
-const morgan = require('morgan');
+const morgan  = require('morgan');
+const cors    = require('cors');
+const app     = express();
+const pino    = require('express-pino-logger')()
 
+app.use(pino);
+
+// Runtime config from .env file
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 
-// setup logging maybe
+// Setup logger
 process.env.LOGGER && app.use(morgan(process.env.LOGGER));
 
-// trust the nginx proxy
+// Trust the nginx proxy
 app.set('trust proxy', true);
 
-// only route
+// Setup CORS
+app.use(cors());
+
+
+// Our only route
 app.get('/', (req, res) => {
-    res.send(`Hi Ben! I got HTTPS working: crazy stuff. Running express on ${port}`);
+    req.log.info('something');
+    res.send(`Hi Ryan! I got HTTPS working: crazy stuff. Running express on ${port}`);
 })
 
+app.get('/products/:id', (req, res) => {
+    req.params.id == 42 && req.log.warn('UHOH')
+    res.json({msg: 'This is CORS-enabled', id: req.params.id})
+})
+
+app.get('/error', (req, res, next) => {
+    next(new Error('kaboom'));
+})
+
+// Catch-all error handler
+app.use((err, req, res, next) => {
+    req.log.error(err);
+    res.statusCode = 500;
+    res.end('error');
+});
+
+// Start listning
 app.listen(port, () => {
     console.log(`Running on ${port}.`);
 })
-
-
