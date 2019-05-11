f430307350ddb3c74c1b1eeed7845cb378b396b4
Scott Stanfield <scottstanfield@gmail.com>
2019-05-11 12:56:40 -0700

    Generate a 404

--- tesst.sh
+++ tesst.sh
@@ -3,3 +3,4 @@ http http://sts2.albina.dev
 http https://sts2.albina.dev/error
 http https://sts2.albina.dev/products/40
 http https://sts2.albina.dev/products/42
+http https://sts2.albina.dev/missing
